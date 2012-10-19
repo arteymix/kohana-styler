2691225dc0d9fe85b0064039183654e90fc048c6
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-10-19 10:08:51 -0400

    Cleanup

--- init.php
+++ init.php
@@ -1,8 +1,6 @@
 <?php
 
 // Importing html and css parsers
-require_once Kohana::find_file("vendor", "htmlparser/simple_html_dom");
-require_once Kohana::find_file("vendor", "cssparser/lib/Sabberworm/CSS/Parser");
-
-
+require_once Kohana::find_file("vendor", "htmlparser/hmlparser");
+require_once Kohana::find_file("vendor", "Sabberworm/CSS/Parser");
 ?>
