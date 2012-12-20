999e6747cb68781d5dc675122898fd3db8aeae5f
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-12-20 00:52:38 -0500

    Did some sutff.

--- README.md
+++
@@ -1,1 +0,0 @@
-Module designed to bind a css stylesheet within html content in style attributes. This way, html documents become easily transportable and presentable through mail system.
--- init.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Importing html and css parsers
-require_once Kohana::find_file("vendor", "htmlparser/hmlparser");
-require_once Kohana::find_file("vendor", "Sabberworm/CSS/Parser");
-?>
